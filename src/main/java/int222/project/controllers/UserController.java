c2c723cb2f2bfe94353d79ecb0938b87cdcd1293
xsolagul <mark_kuuga@hotmail.com>
2021-11-10 18:14:32 +0700

    fix username exception

--- src/main/java/int222/project/controllers/UserController.java
+++ src/main/java/int222/project/controllers/UserController.java
@@ -41,9 +41,9 @@ public class UserController {
 
 	@PostMapping("/register")
 	public User postUser(@RequestBody User user) {// orrequestBody
-		if(userRepo.findByUserName(user.getUserName())!= null){
+		if(!userRepo.findByUserName(user.getUserName()).isEmpty()){
 			throw new AllException(ExceptionResponse.ERROR_CODE.NAME_DUPLICATE,
-					"this name:" + user.getUserName() + " has been use pls change user name!!");
+					"this name:" + user.getUserName() + " has been use pls change user name!!"  );
 		}
 		return userRepo.save(user);
 	}
@@ -55,7 +55,7 @@ public class UserController {
 		if(!userOld.getUserName().equals(authen.getName())) {
 			throw new AllException(ExceptionResponse.ERROR_CODE.USER_NOT_MATCH, "please edit your account only" );
 		}
-		if(user.getUserName() != userOld.getUserName() &&userRepo.findByUserName(user.getUserName())!= null){
+		if(user.getUserName() != userOld.getUserName() &&!userRepo.findByUserName(user.getUserName()).isEmpty()){
 			throw new AllException(ExceptionResponse.ERROR_CODE.NAME_DUPLICATE,
 					"this name:" + user.getUserName() + " has been use. pls change user name!!");
 		}
@@ -127,7 +127,7 @@ public class UserController {
 	public User editUser(@RequestBody User user) {
 		User userOld = userRepo.findById(user.getUserId()).get();
 		
-		if(user.getUserName() != userOld.getUserName() &&userRepo.findByUserName(user.getUserName())!= null){
+		if(user.getUserName() != userOld.getUserName() &&!userRepo.findByUserName(user.getUserName()).isEmpty()){
 			throw new AllException(ExceptionResponse.ERROR_CODE.NAME_DUPLICATE,
 					"this name:" + user.getUserName() + " has been use. pls change user name!!");
 		}
