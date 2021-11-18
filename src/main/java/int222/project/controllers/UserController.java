6656926de53c258bc731edd973acd7f4823da428
xsolagul <mark_kuuga@hotmail.com>
2021-11-18 14:26:16 +0700

    fix admin edit user

--- src/main/java/int222/project/controllers/UserController.java
+++ src/main/java/int222/project/controllers/UserController.java
@@ -143,10 +143,10 @@ public class UserController {
 //					"this password is not match pls enter u old password!!");
 //		}
 		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-		if(user.getPassword()!= null || !passwordEncoder.matches(user.getPassword(), userOld.getPassword())) {
+		if(user.getPassword() != null && !passwordEncoder.matches(user.getPassword(), userOld.getPassword()) && !user.getPassword().equals(userOld.getPassword()) ){
 		String pw = passwordEncoder.encode(user.getPassword());
 		userOld.setPassword(pw);
-		}
+			}
 		userOld.setUserName(user.getUserName());
 		
 		userOld.setAddress(user.getAddress());
